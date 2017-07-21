8d70536937f2c7812f2d353b7e9d32c9bedf4ee7
SomethingStrString <eerikasykes@outlook.com>
2017-07-21 01:15:50 +0100

    Implemented the basic menu functionality for the introduction menu and tested.

--- HauntedHouse.py
+++ HauntedHouse.py
@@ -9,20 +9,20 @@ from asciiDrawings import *
 
 def main() :
     
-    startMenu()
+    gameIntroduction()
 
 
-def startMenu() :
+def gameIntroduction() :
 
     clearConsole(0)
     
     print('\n')
     print('Welcome to HAUNTED HOUSE, a text based adventure game.\n')
     # Delays in code execution to improve screen output and the user experience.
-    sleep(0.85) 
+    #sleep(0.85) 
     print('You will be presented with numerous choices throughout the game and ' +
           'the choices you make\nwill influence your experience within the game.\n')
-    sleep(3.5)
+    #sleep(3.5)
 
     # Setting the scene for the start of the game.
     print('You\'ve arrived; the long drive to the house felt longer this evening, much ' +
@@ -35,11 +35,11 @@ def startMenu() :
     print('Something doesn\'t feel quite right; unsure if it\'s just the drink and ' +
           'the 2am kebab you slip on your rucksak,\ngrab a holdhall in each hand and look up.')
 
-    sleep(5)
+    #sleep(5)
 
     print('\nThis isn\'t the house you remember...\n\n')
 
-    sleep(0.85)
+    #sleep(0.85)
          
     input('~ Press Enter to begin the adventure ~')
 
@@ -49,6 +49,61 @@ def startMenu() :
     # Descibre the weather, surroundings, the house and present the user the their first
     # options - TODO
 
+    print('It\'s stormy and foggy; you\'re already soaked through with the rain. but ' +
+          'you can tell this isn\'t your family home.\nBeing away travelling since ' +
+          'finishing University you expected to see some changes, afterall you\'ve ' +
+          'been away for\nalmost a year now... despite that, this is\'t your home...\n'
+          )
+
+    print('What it is though is shelter, if this truly isn\'t home; it\'s possible you ' +
+          'have no idea where you are, you\'re still\na little drunk, freezing cold and ' +
+          'it\'s now approaching 2:30am! What are you going to do...\n'
+          )
+
+    gameIntroductionMenu()
+
+def gameIntroductionMenu() :
+
+    varMenu = True
+
+    while varMenu :
+        print('Here are your options:\n')
+        print('1) Walk up to the front door')
+        print('2) Walk up to the garage door')
+        print('3) Walk up to the garden gate, it appears to be open')
+        print('4) Use your mobile phone')
+        print('5) Run away, back down the drive (Exit the game)\n')
+
+        userSelects = input('~ Please enter your (number) choice and press Enter ~\n')
+
+        if userSelects == '1' :
+            clearConsole(0)
+            print('\nTesting - walk to front door')
+            varMenu = False
+
+        elif userSelects == '2' :
+            clearConsole(0)
+            print('\nTesting - walk to garage door')
+            varMenu = False
+
+        elif userSelects == '3' :
+            clearConsole(0)
+            print('\nTesting - walk to garden gate')
+            varMenu = False
+
+        elif userSelects == '4' :
+            clearConsole(0)
+            print('\nTesting - use mobile phone')
+            varMenu = False
+
+        elif userSelects == '5' :
+            print('\nTesting - run away and exit game')
+            varMenu = False
+
+        else :
+            #TODO implement ability to exit program after x number of errors are given here
+            print('\nSorry; that was an invalid option, please try again\n')
+            varMenu = True
 
 
 if __name__ == "__main__" :
