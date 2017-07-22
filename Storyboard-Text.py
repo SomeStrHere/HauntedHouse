23ea16ddf01e9f0b75b24262f5bb585548b8f1cb
SomethingStrString <eerikasykes@outlook.com>
2017-07-22 16:15:58 +0100

    Started drafting story board text to set the scene for the player in Storyboard-Text.py

---
+++ Storyboard-Text.py
@@ -0,0 +1,49 @@
+# This file contains story board text segments for use within the game.
+# Currently setup here as a place holder so text segments can be moved to correct place when required.
+
+#
+# Level = Start, player selects option 1 - walk up to the front door
+# 
+print('\nYou walk up to the front door...\n')
+print('The light from the lampost on the drive barley reaches the front door; you can make out ' +
+      'the shape of the door, and windows but little more, the house is in darkness.')
+
+
+#
+# Level = Start, player selects option 2 - walk to garage door
+#
+print('\nYou walk to garage door...\n')
+
+
+#
+# Level = Start, player selects option 3 - walk to garden gate
+#
+print('\nYou walk to the garden gate...\n')
+
+
+#
+# Level = Start, player selects option 4 - player opts to use mobile phone to call for help
+#
+            #elif roll == 3 or roll == 4 :
+            #    print('You pull out your phone... and it\'s dead!\n' +
+            #          'Well that sucks! You quickly think about your options...\n\n' +
+            #          'You can\'t decide whether to knock on the door so early in the ' +
+            #          'morning, loaded down with bags and smelling of drink\nor to see if ' +
+            #          'there would be room to pitch your tent in the garden, and hope ' +
+            #          'the owners don\'t mind.\n'
+            #          )
+
+            #    print('You decide to flip a coin; \'Heads\' you reluctantly knock on the door ' +
+            #          'and ask for help. If it\'s \'Tails\'; you go\n check out the garden ' +
+            #          'through what looks like an open gate.')
+print('\nYou decide to use your phone and call for help.')
+# Print outs for option 4 on the start menu are already included in HauntedHouse.py
+
+# If coin flip result is Heads
+('You walk up to the front door and...') 
+# We could use the same statements for option 1 on start menu
+# or create some new statements for this deviation
+
+
+# If coin flip result is Tails
+print('You walk up to the garden gate and...')
