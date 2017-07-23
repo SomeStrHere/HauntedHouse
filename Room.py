f0dff915540298c035a103a0aebd3b5f86f5ae01
Kieran Jennings <k.jennings@kjfj.co.uk>
2017-07-23 13:36:20 +0100

    Created a Romm class to represents each room

---
+++ Room.py
@@ -0,0 +1,29 @@
+class Room :
+
+    def __init__(self, levelName, floor, roof, lighting, lightType) :
+        self.levelName = levelName
+        self.floor = floor
+        self.roof = roof
+        self.lighting = lighting
+        self.lightType = lightType
+
+        self.connectedRooms = []
+        self.properties = {}
+
+        self.visited = False
+
+    def checkVisited(self) :
+        return self.visited
+
+    def setAsVisited(self) :
+        self.visited = True;
+
+    def setConnectedRooms(self, rooms) :
+        self.connectedRooms = connectedRooms
+
+    def setProperties(self, properties) :
+        self.properties = properties
+
+    def setPropertyValue(self, property, value) :
+        self.properties[property] = value
+
